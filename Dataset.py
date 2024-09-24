07b81a039b92f9e100f721273ee2c3759c7d4607
Park, Su Young <suyoung.park1121@gmail.com>
2024-09-24 14:06:33 +0900

    feature : add "is_inference" in Dataset.py

--- Dataset.py
+++ Dataset.py
@@ -1,7 +1,7 @@
 import torch # type: ignore
 from torch.utils.data import DataLoader # type: ignore
 from torchvision.io import read_image # type: ignore
-from typing import Union, Tuple
+from typing import Union, Tuple, Callable
 import pandas as pd # type: ignore
 import os
 
@@ -9,11 +9,17 @@ class ImageDataset(torch.utils.data.Dataset):
     def __init__(
         self, 
         root_dir, 
-        info_df: pd.DataFrame, # including labels & path 
+        info_df : pd.DataFrame, # including labels & path
+        transform : Callable,
+        is_inference : bool = False
     ):
         self.root_dir = root_dir
-        self.image_labels = info_df['lable'].tolist()
         self.image_paths = info_df['image_path'].tolist()
+        self.transform = transform
+        self.is_inference = is_inference
+
+        if not self.is_inference:
+            self.image_labels = info_df['lable'].tolist()
         
     def __len__(self) -> int: # number of images 
         return len(self.image_labels)
@@ -21,8 +27,12 @@ class ImageDataset(torch.utils.data.Dataset):
     def __getitem__(self, index: int) -> Union[Tuple[torch.Tensor, int], torch.Tensor]:
         img_path = os.path.join(self.root_dir, self.image_paths[index])
         image = read_image(img_path)
-        label = self.image_labels[index]
-        return image, label  
+        
+        if self.is_inference:
+            return image
+        else:
+            label = self.image_labels[index]
+            return image, label  
 
 train_dataset = ImageDataset(root_dir="train_data_root")
 test_dataset = ImageDataset(root_dir="test_data_root")
